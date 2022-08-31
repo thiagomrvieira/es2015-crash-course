d59976270269b780ca45860ee79f54906ace75b2
thiagomrvieira <thiagomrvieira@gmail.com>
2022-08-31 11:51:17 +0100

    Rest and Spread

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-function applyDiscount(cost, discount = .10) {
-    return cost - (cost * discount);
+// Rest operator
+function sumRest(...numbers) {
+    return numbers.reduce((prev, current) => prev + current)
 }
 
-alert(applyDiscount(100, .20));
-alert(applyDiscount(100));
+alert('rest:' + sumRest(1, 2, 3));
+
+// Spread operator
+function sumSpread(x, y) {
+    return numbers.reduce((prev, current) => prev + current)
+}
+
+let numbers = [1, 2];
+alert('spread:' + sumSpread(...numbers));
