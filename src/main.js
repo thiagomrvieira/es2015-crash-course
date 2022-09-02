64b8bf8642fb9a92488dbe2ac1167ba09f1003f4
thiagomrvieira <thiagomrvieira@gmail.com>
2022-09-02 11:02:27 +0100

    Awesome Object Enhancements

--- src/main.js
+++ src/main.js
@@ -1,9 +1,49 @@
-let name = 'Bar';
+// Object Shorthand
 
-let template = `
-    <div class="alert">
-        <p>${name}</p>
-    </div>
-`;
+// function getPerson() {
+//     let name = 'Thiago';
+//     let age = 34;
 
-console.log(template);
+//     return { 
+//         name, 
+//         age,
+        
+//         //  ES5
+//         // greet: function () { 
+//         //     return 'Hello, ' + this.name;
+//         // }
+        
+//         //  ES6
+//         greet: () => `Hello, ${name}`
+
+//     };
+// }
+
+// alert(getPerson().greet());
+
+
+// OBJECT DESTRUCTURING
+// let data = {
+//     results: ['foo', 'bar'],
+//     count: 30
+// }
+
+// ES5
+// let results = data.results;
+// let count = data.count
+// ES6
+// let { results, count } = data;
+
+// console.log(results, count);
+
+
+// OBJECT DESTRUCTURING AS A FUNCTION ARGUMENT
+
+function greet({name, age}) {
+    console.log(`my name is ${name} and i am ${age} years old`);
+}
+
+greet({
+    name: 'Thiago',
+    age: 34
+});
